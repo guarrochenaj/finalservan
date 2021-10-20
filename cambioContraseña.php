870f222dcf081bc34f30db33b028828af9bddc9c
guarrochenaj <guarrochena.julian@gmail.com>
2021-10-20 15:10:41 -0300

    Agrego paginas cambio contraseña y modificar datos, (no funcionan)

--- cambioContraseña.php
+++ cambioContraseña.php
@@ -3,13 +3,20 @@ include_once('conexion.php');
 
 // Existe usuario?
 if(!isset($_SESSION['user_nombre_completo'])){
-        // Reenvio al usuario para finalizar el login
-       $forw = 'login.php';
-       header(sprintf("Location: %s", $forw));
+    // Reenvio al usuario para finalizar el login
+    $forw = 'login.php';
+    header(sprintf("Location: %s", $forw));
+
 }
 
 
-
+if(isset($_POST['contraseña']) && $_POST['contrasena'] == $_POST['contrasena2']){
+$query ="UPDATE 'edibis.usuarios' 
+        SET 'Contrasena' = '.$_POST['contrasena'].' 
+        WHERE Dni = '$_SESSION['user_id']'";
+$stmt = $cnPDO->prepare($query);
+$stmt->execute();
+};
 ?>
 
 <!DOCTYPE html>
@@ -17,33 +24,25 @@ if(!isset($_SESSION['user_nombre_completo'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edibis | Home</title>
+    <title>Edibis | Nuevo Usuario</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
-    
-    </head>
+</head>
 
-    <?php include_once "nav.php";?>
+<?php include_once "nav.php";?>
 
-    <body>
+<body>
+    <form name="nueva_contraseña" id="nueva_contraseña" method="post">
+     
+     Nueva Contraseña: <input type="password" required id="contrasena" name="contrasena" class="m-1"><br>
+     Confirme Contraseña:<input type="password" required id="contrasena2" name="contrasena2" class="m-1"><br>
 
-    <p><b>Numero de documento:</b> <?php echo $_SESSION['user_id'] ?></p>
-    <p><b>Usuario:</b> <?php echo $_SESSION['user'] ?></p>
-    <p><b>Nombre:</b> <?php echo $_SESSION['user_nombre'] ?></p>
-    <p><b>Apellido:</b> <?php echo $_SESSION['user_apellido'] ?></p>
-    <p><b>Email:</b> <?php echo $_SESSION['user_email'] ?></p>
-    <p><b>Fecha de nacimiento:</b> <?php echo $_SESSION['f_nacimiento'] ?></p>
-    <p><b>Pais:</b> <?php echo $_SESSION['pais'] ?></p>
-    <p><b>Provincia:</b> <?php echo $_SESSION['provincia'] ?></p>
-    <p><b>Ciudad:</b> <?php echo $_SESSION['ciudad'] ?></p>
-    <p><b>Direccion:</b> <?php echo  $_SESSION['direccion']?></p>
+     <input type="submit" class="m-2"><input type="reset" class="m-1">
+     </form>
 
-    <a href="modificarDatos.php" class="btn btn-primary p-3 m-3">Modificar<br>Datos</a>
-    <a href="cambioContraseña.php" class="btn btn-primary p-3 m-3">Modificar<br>Contraseña</a>
-
-    </body>
+</body>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
